6aa6d0a9cd64b1ca760df30c3d9d73aa73464f5b
humbertodias <humberto.dias@techne.com.br>
2015-10-22 19:00:03 -0200

    URI 1929 Solution

---
+++ trainning/uri/Extremamente Básico/1929/Main.c
@@ -0,0 +1,31 @@
+#include <stdio.h> // printf
+#include <string.h> // strtok
+#include <stdlib.h> // atoi
+#include <math.h> // abs
+
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+     char linha[400];
+     scanf("%[^\n]%*c", linha);
+     // [^\n] is accept other than '\ n'
+     // %*c is ignore one character('\n')
+
+     int a = atoi(strtok(linha, " "));
+     int b = atoi(strtok(NULL, " "));
+     int c = atoi(strtok(NULL, " "));
+     int d = atoi(strtok(NULL, " "));
+
+     int tenta1 = (abs(b-c) < a && a < (b+c)) || (abs(a-c) < b && b < (a+c)) || (abs(a-b) < c && c < (a+b)); // a b c
+     int tenta2 = (abs(c-d) < b && b < (c+d)) || (abs(b-d) < c && c < (b+d)) || (abs(b-c) < d && d < (b+c)); // b c d
+     int tenta3 = (abs(c-d) < a && a < (c+d)) || (abs(a-d) < c && c < (a+d)) || (abs(a-c) < d && d < (a+c)); // a c d
+     int tenta4 = (abs(b-d) < a && a < (b+d)) || (abs(a-d) < b && b < (a+d)) || (abs(a-b) < d && d < (a+b)); // a b d
+
+     printf("%c\n", (tenta1 || tenta2 || tenta3 || tenta4) ? 'S' : 'N');
+     return 0;
+}
---
+++ trainning/uri/Extremamente Básico/1929/Main3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+a,b,c,d = [int(s) for s in input().split(' ')]
+tenta1 = (abs(b-c) < a and a < (b+c)) or (abs(a-c) < b and b < (a+c)) or (abs(a-b) < c and c < (a+b)) # a b c
+tenta2 = (abs(c-d) < b and b < (c+d)) or (abs(b-d) < c and c < (b+d)) or (abs(b-c) < d and d < (b+c)) # b c d
+tenta3 = (abs(c-d) < a and a < (c+d)) or (abs(a-d) < c and c < (a+d)) or (abs(a-c) < d and d < (a+c)) # a c d
+tenta4 = (abs(b-d) < a and a < (b+d)) or (abs(a-d) < b and b < (a+d)) or (abs(a-b) < d and d < (a+b)) # a b d
+
+if tenta1 or tenta2 or tenta3 or tenta4:
+    print("S")
+else: 
+    print("N")
