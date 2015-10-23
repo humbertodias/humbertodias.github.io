4d2d8c7b2eec404b12f70f4e203f8699ebd9f4a2
humbertodias <humberto.dias@techne.com.br>
2015-10-23 13:55:41 -0200

    Vagrant com pygame

---
+++ vagrant/pygame/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Set start time so we know how long the bootstrap takes
+T="$(date +%s)"
+
+# Dependences
+sudo apt-get install xvfb python-pygame unzip
+
+echo 'Downloading and copying the Game to /vagrant'
+cd /vagrant
+wget "https://raw.githubusercontent.com/humbertodias/humbertodias.github.io/master/vagrant/pygame/asteroids_sprites.zip"
+unzip asteroids_sprites.zip
+
+cd asteroids_sprites
+python asteroids.py
+
+# Set start time so we know how long the bootstrap takes
+T="$(date +%s)"
