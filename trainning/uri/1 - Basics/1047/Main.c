b5c0e31da1bff95c6b47369a7dbab04cf348e609
humbertodias <humberto.dias@techne.com.br>
2015-10-28 01:12:38 -0200

    1046, 1047, 1048, 1049

---
+++ trainning/uri/1 - Basics/1047/Main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c, d;
+    
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    
+    int inicio = a*60 + b;
+    int final = c*60 + d;
+    int duracao = 0;
+    
+    if(a <= c){
+      duracao = final - inicio;
+      if(duracao == 0)
+          printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",24,duracao%60);
+      else
+          printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",(duracao - duracao%60)/60,duracao%60);
+    }else{
+      duracao = (24*60 - inicio) + final;
+      printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",(duracao - duracao%60)/60,duracao%60);
+    }
+    
+    return 0;
+    
+}
