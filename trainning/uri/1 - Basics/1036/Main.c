0db87ba8f72be53bdf6e32283f0e2f77fa05fee8
humbertodias <humberto.dias@techne.com.br>
2015-10-28 00:17:24 -0200

    1036

---
+++ trainning/uri/1 - Basics/1036/Main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a, b, c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if((pow(b,2) - 4*a*c) < 0 || 2*a == 0)
+         printf("Impossivel calcular\n");
+    else{
+         printf("R1 = %.5lf\n",((-b) + sqrt((pow(b,2) - 4*a*c)))/(2*a));
+         printf("R2 = %.5lf\n",((-b) - sqrt((pow(b,2) - 4*a*c)))/(2*a));
+     }
+    return 0;
+}
