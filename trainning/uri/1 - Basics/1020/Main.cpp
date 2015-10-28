e973f494e6d70272abd8a20780ed77d846ce4a35
humbertodias <humberto.dias@techne.com.br>
2015-10-27 23:04:28 -0200

    1020

---
+++ trainning/uri/1 - Basics/1020/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    
+    cin >> a;
+    cout << a/365 << " ano(s)" << endl;
+    cout << (a%365)/30 << " mes(es)" << endl;
+    cout << (a%365)%30 << " dia(s)" << endl;
+    return 0;
+}
