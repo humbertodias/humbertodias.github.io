6ab396431d3a25a97c7cbaaf358d82d4a85fc820
humbertodias <humberto.dias@techne.com.br>
2015-10-28 16:29:01 -0200

    Fix

--- trainning/uri/1 - Basics/1837/Main.cpp
+++ trainning/uri/1 - Basics/1837/Main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
-
 using namespace std;
-
-int main () {
-
-    int a, b, q, r;
-
+int main() {
+    int a, b;
     cin >> a >> b;
-
-    q = a / b;
-    r = a % b;
-
-    if (r < 0){
-    
-        if ( q > 0)
-            q ++;
-
-        if(q< 0)
-            q --;
-
-        r = a -(q *b);
-
+    int q=a/b;
+    int r=a%b;
+    if(a < 0 && b < 0)
+    {
+        if(r<0 && q<=0)
+        {
+            q++;
+            r=-(b*q)+a;
+        }
+        else if(r<0 && q>0)
+        {
+            q++;
+            r=-(b*q)+a;
+        }
     }
-
+    else
+    {
+        if(r<0 && q<=0)
+        {
+            q--;
+            r=-(b*q)+a;
+        }
+        else if(r<0 && q>0)
+        {
+            q++;
+            r=-(b*q)+a;
+        }
+    }
     cout << q << " " << r << endl;
-
+    return 0;
 }
