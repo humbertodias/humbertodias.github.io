f5aae90f38fb7562d2c714986eca5ff6975016bc
humbertodias <humberto.dias@techne.com.br>
2015-10-29 11:36:26 -0200

    fix

---
+++ trainning/uri/1 - Basics/1845/Main3.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+letter = ['s','j','b','z','p','v','x',"S","J","B","Z","P","V","X"]
+
+while True:
+    try:
+        a = input()
+        b = a[::]
+    except EOFError:
+        break
+    for l in a:
+        if l in letter:
+            b = b.replace(l.upper(),'F')
+            b = b.replace(l,'f')
+            b = b.replace('ff','f')
+            b = b.replace('Ff','F')
+            b = b.replace('fF','f')
+            a = b.replace('FF','F')
+    b = b.replace('ff','f')
+    b = b.replace('Ff','F')
+    b = b.replace('fF','f')
+    a = b.replace('FF','F')
+    print(a)
