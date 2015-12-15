6cabbf05eae5a977428495d657a345bd177ac03b
humbertodias <humberto.dias@techne.com.br>
2015-12-15 00:48:13 -0200

    Separando projeto

--- games/asteroids_presentation/bullet.py
+++
@@ -1,9 +0,0 @@
-from movable_sprite import *
-
-class Bullet(MovableSprite):
-    def update(self):
-        MovableSprite.update(self)
-        self.up()
-
-        if self.is_out_of_screen():
-            self.kill()
